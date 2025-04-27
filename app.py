d5ca1000c90d9a4f884009ca20da700253538ed3
spycat111 <spycat111@gmail.com>
2025-04-27 20:27:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__, static_folder=None)
 SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
 SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
 SMTP_USER = os.getenv("SMTP_USER", "spycat111@gmail.com")
-SMTP_PASS = os.getenv("SMTP_PASS", "4kQsspEm7")
+SMTP_PASS = os.getenv("SMTP_PASS", "")
 TO_EMAIL   = os.getenv("TO_EMAIL", "spycat111@gmail.com")
 
 # ——— Serve index.html at the root URL —————————————————————————
